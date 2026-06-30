cd8cde18a7999a76793aa20f4a7370e059f46ff9
HGILL241 <hareetgill19@gmail.com>
2026-06-30 17:19:27 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,10 +4,11 @@
 export const USE_FIREBASE = true;
 
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyBiPrIG9NewNNwXNaESQJABDy5kz4M9Ex0",
+  authDomain: "canada-day-game.firebaseapp.com",
+  projectId: "canada-day-game",
+  storageBucket: "canada-day-game.firebasestorage.app",
+  messagingSenderId: "633488741465",
+  appId: "1:633488741465:web:dc2dbef49f9a352cec8d48",
+  measurementId: "G-V0FBE7JJGZ"
 };
